5c68b4325d2136fae3f0c8b856501b66d724d017
eduardChalovski <85243017+eduardChalovski@users.noreply.github.com>
2021-06-07 19:45:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
+    let mtoken =  "bEdoZZXsDIlm";
 
+    $("h1").click(function() {
+        $.ajax({
+            type: "GET",
+            url: "http://18.197.21.42:8080/channels/",
+            headers:{
+                "X-Group-Token": mtoken
+            },
+            contentType: "applicatoin/json",
+            success: function(content, status) {alert(JSON.stringify(content));},
+            error: function() {
+                alert("error yay")
+            }
+        });
+    });
 });
